5c99457802c1861167f3e74e8c1faaf18dd3e949
Anuska Gurung <annggrg@gmail.com>
2024-05-14 11:15:14 +0545

    first commit

---
+++ project_one/lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          title: Text("App Bar"),
+        ),
+        body: const Text(
+          "hello",
+          style: TextStyle(fontSize: 53, fontWeight: FontWeight.w300),
+        ));
+  }
+}
